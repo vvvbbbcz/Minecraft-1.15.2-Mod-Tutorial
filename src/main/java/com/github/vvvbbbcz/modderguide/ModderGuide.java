2db2cc56b5200444c616e4998c5e82df7bad52ad
vvvbbbcz <vvvbbbcz@163.com>
2020-09-02 18:19:13 +0800

    完成3.1

---
+++ src/main/java/com/github/vvvbbbcz/modderguide/ModderGuide.java
@@ -0,0 +1,38 @@
+package com.github.vvvbbbcz.modderguide;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("modderguide")
+public class ModderGuide {
+	public ModderGuide() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onServerStarting);
+
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+	}
+
+	private void doClientStuff(final FMLClientSetupEvent event) {
+	}
+
+	private void onServerStarting(final FMLServerStartingEvent event) {
+	}
+
+	private void enqueueIMC(final InterModEnqueueEvent event) {
+	}
+
+	private void processIMC(final InterModProcessEvent event) {
+	}
+}
