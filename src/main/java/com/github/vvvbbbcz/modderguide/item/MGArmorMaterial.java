4886eb2f730a666752480ea4ffb9bc2bb2d30029
vvvbbbcz <vvvbbbcz@163.com>
2020-09-04 23:05:14 +0800

    完成3.2.3前半部分

---
+++ src/main/java/com/github/vvvbbbcz/modderguide/item/MGArmorMaterial.java
@@ -0,0 +1,67 @@
+package com.github.vvvbbbcz.modderguide.item;
+
+import com.github.vvvbbbcz.modderguide.ModderGuide;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+public enum MGArmorMaterial implements IArmorMaterial {
+	EMERALD("emerald", 25, new int[]{3, 6, 8, 3}, 12, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, () -> Ingredient.fromItems(Items.EMERALD));
+
+	private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+	private final String name;
+	private final int maxDamageFactor;
+	private final int[] damageReductionAmountArray;
+	private final int enchantability;
+	private final SoundEvent soundEvent;
+	private final float toughness;
+	private final LazyValue<Ingredient> repairMaterial;
+
+	MGArmorMaterial(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountsIn, int enchantabilityIn, SoundEvent equipSoundIn, float toughnessIn, Supplier<Ingredient> repairMaterialSupplier) {
+		this.name = nameIn;
+		this.maxDamageFactor = maxDamageFactorIn;
+		this.damageReductionAmountArray = damageReductionAmountsIn;
+		this.enchantability = enchantabilityIn;
+		this.soundEvent = equipSoundIn;
+		this.toughness = toughnessIn;
+		this.repairMaterial = new LazyValue<>(repairMaterialSupplier);
+	}
+
+	public int getDurability(EquipmentSlotType slotIn) {
+		return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+	}
+
+	public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+		return this.damageReductionAmountArray[slotIn.getIndex()];
+	}
+
+	public int getEnchantability() {
+		return this.enchantability;
+	}
+
+	public SoundEvent getSoundEvent() {
+		return this.soundEvent;
+	}
+
+	public Ingredient getRepairMaterial() {
+		return this.repairMaterial.getValue();
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public String getName() {
+		return ModderGuide.MODID + ":" + this.name;
+	}
+
+	public float getToughness() {
+		return this.toughness;
+	}
+
+}
--- src/main/java/com/github/vvvbbbcz/modderguide/item/MGItems.java
+++ src/main/java/com/github/vvvbbbcz/modderguide/item/MGItems.java
@@ -1,8 +1,11 @@
 package com.github.vvvbbbcz.modderguide.item;
 
 import com.github.vvvbbbcz.modderguide.ModderGuide;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,4 +13,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class MGItems {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ModderGuide.MODID);
 	public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> EMERALD_SWORD = ITEMS.register("emerald_sword", () -> new SwordItem(MGItemTier.EMERALD, 3, -2.0F, new Item.Properties().group(ItemGroup.COMBAT)));
+	public static final RegistryObject<Item> EMERALD_HELMET = ITEMS.register("emerald_helmet", () -> new ArmorItem(MGArmorMaterial.EMERALD, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)));
 }
